e817cfc2b7d156cc9129e679a2ffae24e92b7c36
Praneeth Reddy <rpraneeth.b@gmail.com>
2025-07-10 10:54:29 +0530

    Program for Palindrome and missing closing Index

---
+++ src/main/java/com/programs/miscellaneous/MissingClosingIndex.java
@@ -0,0 +1,28 @@
+package com.programs.miscellaneous;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class MissingClosingIndex {
+    public static void main(String[] args) {
+        //String input = "([]){}([{})";
+        String input = "([{})";
+        List<Integer> missingIndex = new ArrayList<Integer>();
+        Stack<Integer> stack = new Stack<Integer>();
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            if (ch == '(' || ch == '{' || ch == '[') {
+                stack.push(i);
+            } else if (ch == ')' || ch == '}' || ch == ']') {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+            }
+        }
+        while (!stack.isEmpty()) {
+            missingIndex.add(stack.pop());
+        }
+        System.out.println("Missing Indices: " + missingIndex);
+    }
+}
